45c39584cf6324fcdbb62359a3110d59183701a8
Velvet <satoshinji24@gmail.com>
2026-04-27 01:33:05 +0800

    Initial commit

---
+++ public/scripts/punkman.js
@@ -0,0 +1,224 @@
+const Punkman = (function(){
+    let user = null;
+    let inGame = false;
+
+    const initJoinPage = function(){
+        $("#join-button").on("click", function(e){
+            e.preventDefault();
+
+            socket.emit("join", user.name);
+        });
+
+        socket.on("join_error", (error) => {
+            $("#join-message").text(error);
+        });
+
+        socket.on("join_success", () => {
+            $("#join-page").hide();
+            $("#wait-page").show();
+
+            initWaitPage();
+            inGame = true;
+        });
+    };
+
+    const initWaitPage = function(){
+        socket.on("update_players", (players) => {
+            if(!inGame)
+                return;
+            updatePlayers(players);
+        });
+
+        $("#ready-button").on("click", function(e){
+            socket.emit("ready");
+            $("#ready-button").hide();
+            $("#game-message").text("Please wait for the other players...");
+        });
+
+        socket.on("game_start", () => {
+            if(!inGame)
+                return;
+
+            $("#wait-page").hide();
+            $("#main-page").show();
+        });
+
+        socket.on("game_end", (players) => {
+            if(!inGame)
+                return;
+            updatePlayers(players);
+
+            inGame = false;
+        });
+    };
+
+    const updatePlayers = function(players){
+        $(".player").hide();
+
+        let index = 1;
+        for (const id in players) {
+            const player = players[id];
+            let name = "You are";
+            if (id != socket.id)
+                name = `${player["name"]} is`;
+            let status = "";
+            if (player["sign"])
+                status = `${name} ${player["sign"]}!`;
+            else if (player["ready"])
+                status = `${name} ready!`;
+            else
+                status = `${name} not yet ready!`;
+            $("#player" + index).find(".status").text(status);
+
+            $("#player" + index).show();
+
+            index++;
+        }
+    };
+
+    const init = function(){
+        socket = io();
+
+        socket.on("connect_error", (error) => {
+            $("#connect-message").text(error.message);
+        });
+
+        socket.on("connect", () => {
+            $("#connect-page").hide();
+            $("#start-page").show();
+            validate();
+        });
+
+        socket.on("disconnect", () => {
+            window.location.reload();
+        });
+
+        Avatar.populate($("#register-avatar"));
+
+        $("#start-signin-button").on("click", function() {
+            $("#start-page").hide();
+            $("#signin-page").show();
+            $("#signin-form").get(0).reset();
+            $("#signin-message").text("");
+        });
+
+        $("#start-register-button").on("click", function() {
+            $("#start-page").hide();
+            $("#register-page").show();
+            $("#register-form").get(0).reset();
+            $("#register-message").text("");
+        });
+
+        $(".back-button").on("click", function() {
+            $("#start-page").show();
+            $("#signin-page").hide();
+            $("#register-page").hide();
+        });
+
+        $("#register-button").on("click", function(e) {
+            e.preventDefault();
+
+            const username = $("#register-username").val().trim();
+            const avatar   = $("#register-avatar").val();
+            const name     = $("#register-name").val().trim();
+            const password = $("#register-password").val().trim();
+            const confirmPassword = $("#register-confirm").val().trim();
+
+            if (password != confirmPassword) {
+                $("#register-message").text("Passwords do not match.");
+                return;
+            }
+
+            const json = JSON.stringify({ username, avatar, name, password });
+            fetch("/register", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: json
+            })
+            .then((res) => res.json())
+            .then((json) => {
+                if (json.error) {
+                    $("#register-message").text(json.error);
+                    return;
+                }
+                $("#register-form").get(0).reset();
+                $("#register-message").text("Your account has been successfully registered.");
+            })
+            .catch((err) => {
+                $("#register-message").text(err);
+            });
+        });
+
+        $("#signin-button").on("click", function(e) {
+            e.preventDefault();
+            const username = $("#signin-username").val().trim();
+            const password = $("#signin-password").val().trim();
+            const json = JSON.stringify({ username, password });
+            fetch("/signin", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: json
+            })
+            .then((res) => res.json())
+            .then((json) => {
+                if (json.error) {
+                    $("#signin-message").text(json.error);
+                    return;
+                }
+                user = json.user;
+                $("#user-avatar").html(Avatar.getCode(user.avatar));
+                $("#user-name").text(user.name);
+
+                $("#signin-page").hide();
+                $("#join-page").show();
+                initJoinPage();
+            })
+            .catch((err) => {
+                $("#signin-message").text(err);
+            });
+        });
+
+        $("#signout-button").on("click", function(e) {
+            e.preventDefault();
+            fetch("/signout")
+                .then((res) => res.json())
+                .then((json) => {
+                    if (json.error) {
+                        console.log(json.error);
+                        return;
+                    }
+                    user = null;
+                    $("#join-page").hide();
+                    $("#start-page").show();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+    };
+
+    const validate = function(){
+        fetch("/validate")
+        .then((res) => res.json())
+        .then((json) => {
+            console.log("Endpoint '/validate' -", json);
+            if(json.error){
+                console.log(json.error);
+                return;
+            }
+
+            user = json.user;
+            $("#user-avatar").html(Avatar.getCode(user.avatar));
+            $("#user-name").text(user.name);
+
+            $("#start-page").hide();
+            $("#join-page").show();
+            initJoinPage();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
+    return {init};
+})();
---
+++ server.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const session = require("express-session");
+const {createServer} = require("http");
+const {Server} = require("socket.io");
+const argon2 = require("argon2");
+const fs = require("fs");
+
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+const gameSession = session({
+    secret: "game",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {maxAge: 300000}
+});
+app.use(gameSession);
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+let players = {};
+let gameStarted = false;
+
+app.post("/register", async (req, res) => {
+    const {username, avatar, name, password} = req.body;
+    let users = JSON.parse(fs.readFileSync("data/users.json"));
+
+    if(username == "" || avatar == "" || name == "" || password == ""){
+        res.json({error: "Username/avatar/name/password cannot be empty."});
+        return;
+    }
+    if(!/^\w+$/.test(username)){
+        res.json({error: "The username can only contain underscores, letters, or numbers."});
+        return;
+    }
+    if(username in users){
+        res.json({error: "The username already exists."});
+        return;
+    }
+
+    const hash = await argon2.hash(password);
+    users[username] = {
+        "avatar": avatar,
+        "name": name,
+        "password": hash
+    };
+    fs.writeFileSync("data/users.json", JSON.stringify(users, null, 2));
+    res.json({success: true});
+});
+
+app.post("/signin", async (req, res) => {
+    const {username, password} = req.body;
+    let users = JSON.parse(fs.readFileSync("data/users.json"));
+
+    if(username == "" || password == "" || !(username in users)){
+        res.json({error: "Incorrect username/password."});
+        return;
+    }
+
+    const user = users[username];
+
+    const verified = await argon2.verify(user.password, password);
+    if(!verified){
+        res.json({error: "Incorrect username/password."});
+        return;
+    }
+
+    req.session.user = {
+        "username": username,
+        "avatar": user.avatar,
+        "name": user.name
+    }
+
+    res.json({"user": req.session.user});
+});
+
+app.get("/validate", (req, res) => {
+    let user = req.session.user;
+    if(!user){
+        res.json({error: "User does not exist."});
+        return;
+    }
+
+    res.json({"user": user});
+});
+
+app.get("/signout", (req, res) => {
+    if(req.session.user){
+        delete req.session.user;
+    }
+
+    res.json({success: true});
+});
+
+io.on("connection", (socket) => {
+    console.log("Player connected:", socket.id);
+
+    socket.on("join", (name) => {
+        if(gameStarted){
+            socket.emit("join_error", "The game has started.");
+            return;
+        }
+
+        if(Object.keys(players).length == 4){
+            socket.emit("join_error", "The game is full.");
+            return;
+        }
+
+        for(const id in players){
+            const player = players[id];
+            if(player.name == name){
+                socket.emit("join_error", "The name has already been used in the game.");
+                return;
+            }
+        }
+
+        players[socket.id] = {name: name, ready:false};
+        socket.emit("join_success");
+        io.emit("update_players", players);
+        console.log("Current players - ", players);
+    });
+
+    socket.on("ready", () => {
+        players[socket.id]["ready"] = true;
+        io.emit("update_players", players);
+
+        for(const id in players){
+            const player = players[id];
+            if(player.ready == false)
+                return;
+        }
+
+        io.emit("game_start");
+        gameStarted = true;
+    });
+
+    socket.on("disconnect", () => {
+        if(players[socket.id]){
+            delete players[socket.id];
+            io.emit("update_players", players);
+        }
+    });
+});
+
+httpServer.listen(8000, () => {
+    console.log("http://localhost:8000");
+});
